174efd3f764d187e3bb730e4a79ec4ddee8e9bfb
PLuiza12 <pluiza64@gmail.com>
2025-06-22 11:07:37 -0300

    Importando a classe scanner e criando o menu de informações para o usuario criar sua conta no banco digital

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new  Scanner(System.in);
+
+        //Mensagens para o usuário
+        System.out.println("=== Bem-vindo ao Banco Digital ===");
+        System.out.println("Por favor, digite seu nome:  ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite o número da sua conta:  ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Digite o número da agência:  ");
+        int agencia = scanner.nextInt();
+
+        System.out.println("Informe o saldo inicial da conta:  ");
+        double saldo = scanner.nextDouble();
+
+        //Mensagens para o usuario
+        System.out.println("\n=== Conta Criada com Sucesso! ===");
+        System.out.println("Olá " + nomeCliente + ",obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua agência é " + agencia + ", conta número " + numeroConta + " e seu saldo de R$" + saldo + " já está disponível para saque.");
+
+        scanner.close();
     }
 }
