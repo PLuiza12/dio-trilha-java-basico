1c268a2a46551ca6323becff6bb2c8225a8e4e2e
PLuiza12 <pluiza64@gmail.com>
2025-06-22 11:09:32 -0300

    ajustando informações sobre comentários feitos ao codigo

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -4,7 +4,7 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new  Scanner(System.in);
 
-        //Mensagens para o usuário
+        //Mensagens para o usuário criar a conta
         System.out.println("=== Bem-vindo ao Banco Digital ===");
         System.out.println("Por favor, digite seu nome:  ");
         String nomeCliente = scanner.nextLine();
@@ -18,7 +18,7 @@ public class ContaTerminal {
         System.out.println("Informe o saldo inicial da conta:  ");
         double saldo = scanner.nextDouble();
 
-        //Mensagens para o usuario
+        //Mensagens para o usuario verificar as informações que foram solicitadas anteriormente
         System.out.println("\n=== Conta Criada com Sucesso! ===");
         System.out.println("Olá " + nomeCliente + ",obrigado por criar uma conta em nosso banco.");
         System.out.println("Sua agência é " + agencia + ", conta número " + numeroConta + " e seu saldo de R$" + saldo + " já está disponível para saque.");
